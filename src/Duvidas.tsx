dbb57f82a3de4b1b191d74102f3cc37f902bf6b3
jasmincortez <jasmincortez_@outlook.com>
2024-04-07 16:42:37 +0100

    Create: Duvidas list

--- src/Duvidas.tsx
+++ src/Duvidas.tsx
@@ -1,53 +1,61 @@
-import { Avatar, Box, Button, Stack } from "@mui/material";
+import { Box } from "@mui/material";
 import Typography from "@mui/material/Typography";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
-import MusicNoteIcon from "@mui/icons-material/MusicNote";
-import YouTubeIcon from "@mui/icons-material/YouTube";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import EmojiNatureIcon from "@mui/icons-material/EmojiNature";
+import FilterVintageIcon from "@mui/icons-material/FilterVintage";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Duvidas() {
+  const [open, setOpen] = useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
     <Box>
       <Typography variant="h2" sx={{ my: 3 }}>
-        Contact
+        Dúvidas
       </Typography>
 
-      <Stack spacing={6} alignItems="flex-start">
-        <Button
-          target="_blank"
-          component="a"
-          startIcon={<AlternateEmailIcon />}
-        >
-          Email
-        </Button>
-        <Button target="_blank" component="a" startIcon={<InstagramIcon />}>
-          Instagram
-        </Button>
-        <Button
-          target="_blank"
-          component="a"
-          startIcon={
-            <Avatar
-              alt="Spotlight logo"
-              src="./spotlight.jpeg"
-              sx={{ width: "1.25rem", height: "1.25rem" }}
-            />
-          }
-        >
-          Spotlight Performer
-        </Button>
-        <Button target="_blank" component="a" startIcon={<MusicNoteIcon />}>
-          Spotify
-        </Button>
-        <Button
-          target="_blank"
-          component="a"
-          startIcon={<YouTubeIcon />}
-          href="https://www.youtube.com/channel/UCBBEwlCFKcZOVGu1wujKLsg"
-        >
-          YouTube
-        </Button>
-      </Stack>
+      <List
+        sx={{ width: "100%", bgcolor: "background.paper" }}
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+      >
+        {/* FAQ 1 */}
+        <ListItemButton onClick={handleClick}>
+          <ListItemIcon>
+            <FilterVintageIcon />
+          </ListItemIcon>
+          <ListItemText primary="O que é o Kombuchá e como ele é produzido?" />
+          {open ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItemButton sx={{ pl: 4 }}>
+              <ListItemIcon>
+                <EmojiNatureIcon />
+              </ListItemIcon>
+              <ListItemText>
+                {"Saiba mais sobre o que é kombuchá e nossa produção "}
+                <Link color="inherit" to="/o-que-e-kombucha">
+                  aqui!
+                </Link>
+              </ListItemText>
+            </ListItemButton>
+          </List>
+        </Collapse>
+
+        {/* FAQ 2 */}
+      </List>
     </Box>
   );
 }

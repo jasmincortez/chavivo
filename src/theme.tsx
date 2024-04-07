9b7358873faffebf4347f9b98294ebcf509ef7ad
jasmincortez <jasmincortez_@outlook.com>
2024-04-07 11:34:10 +0100

    Change: button lowercase

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,12 +1,12 @@
-import { PaletteColor, createTheme } from "@mui/material/styles"
-import { red } from "@mui/material/colors"
+import { PaletteColor, createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
 
-const tools = createTheme()
-const primary = tools.palette.augmentColor({ color: { main: "#80CDC6" } })
-const secondary = tools.palette.augmentColor({ color: { main: "#a4d8d9" } })
-const containerBg = tools.palette.augmentColor({ color: { main: "#ffffff" } })
-const highlight = tools.palette.augmentColor({ color: { main: "#624cab" } })
-const font = tools.palette.augmentColor({ color: { main: "#000000" } })
+const tools = createTheme();
+const primary = tools.palette.augmentColor({ color: { main: "#80CDC6" } });
+const secondary = tools.palette.augmentColor({ color: { main: "#a4d8d9" } });
+const containerBg = tools.palette.augmentColor({ color: { main: "#ffffff" } });
+const highlight = tools.palette.augmentColor({ color: { main: "#624cab" } });
+const font = tools.palette.augmentColor({ color: { main: "#000000" } });
 
 // A custom theme for this app
 const theme = createTheme({
@@ -41,21 +41,22 @@ const theme = createTheme({
       styleOverrides: {
         root: {
           fontFamily: "Playfair Display",
+          textTransform: "lowercase",
         },
       },
     },
   },
-})
+});
 
-export default theme
+export default theme;
 
 declare module "@mui/material/styles" {
   interface Palette {
-    highlight: PaletteColor
-    font: PaletteColor
+    highlight: PaletteColor;
+    font: PaletteColor;
   }
   interface PaletteOptions {
-    highlight: PaletteColor
-    font: PaletteColor
+    highlight: PaletteColor;
+    font: PaletteColor;
   }
 }

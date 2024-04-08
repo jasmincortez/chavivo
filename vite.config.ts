0b55d61e191d6e27a4c979efed275e3a6037de10
Ed Simmons <e.d.simmons@outlook.com>
2024-04-08 14:45:09 +0100

    Add: "@" path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import { join, parse, resolve } from "path"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   base: "/chavivo/",
   build: {
     rollupOptions: {

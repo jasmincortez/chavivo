574011adbf2659b499c6deed39aa855ea5eca0b1
jasmincortez <jasmincortez_@outlook.com>
2024-04-10 13:31:51 +0100

    Add: start to Sabores

--- src/Sabores.tsx
+++ src/Sabores.tsx
@@ -1,5 +1,22 @@
 import { Box, Typography } from "@mui/material";
+import CarouselSabores from "./components/CarouselSabores";
+
+{
+  /* Bkgnd color split, bottle + info in carousel  */
+}
+
+const images = [
+  "https://www.chavivo.com.br/site/wp-content/uploads/2022/04/3eadeca34cdba8e3fa4d192022143b70-780x1024.png",
+  "https://www.chavivo.com.br/site/wp-content/uploads/2022/04/0cda8e552f79cc73e55cf556935c06fa-893x1024.png",
+  "https://www.chavivo.com.br/site/wp-content/uploads/2022/04/8b2d7abe57e5d89af94efaf3cacbc4bb-790x1024.png",
+  "https://www.chavivo.com.br/site/wp-content/uploads/2022/04/bb8df66fed768cb725664ad8b57ed49c-841x1024.png",
+  "https://www.chavivo.com.br/site/wp-content/uploads/2022/04/090a1877309aa8fb283ea0f8a7718e82-605x1024.png",
+];
 
 export default function Sabores() {
-  return <Box>{/* Bkgnd color split, bottle + info in carousel  */}</Box>;
+  return (
+    <Box>
+      <CarouselSabores images={images} slidesToShow={1} />
+    </Box>
+  );
 }
---
+++ src/components/CarouselSabores.tsx
@@ -0,0 +1,105 @@
+import Slider, { type Settings, type CustomArrowProps } from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Avatar, Box, IconButton } from "@mui/material";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import { useEffect, useRef, useState } from "react";
+
+function NextArrow(props: CustomArrowProps) {
+  return (
+    <Box
+      sx={{
+        position: "absolute",
+        top: "50%",
+        right: "0",
+        transform: "translate(100%, -50%)",
+        zIndex: 99,
+      }}
+    >
+      <IconButton
+        aria-label="next"
+        onClick={props.onClick}
+        sx={{ color: "white" }}
+      >
+        <NavigateNextIcon style={{ fontSize: 60 }} />
+      </IconButton>
+    </Box>
+  );
+}
+
+function PreviousArrow(props: CustomArrowProps) {
+  return (
+    <Box
+      sx={{
+        position: "absolute",
+        top: "50%",
+        transform: "translate(-100%, -50%)",
+        zIndex: 99,
+      }}
+    >
+      <IconButton
+        aria-label="prev"
+        onClick={props.onClick}
+        sx={{ color: "white" }}
+      >
+        <NavigateBeforeIcon style={{ fontSize: 60 }} />
+      </IconButton>
+    </Box>
+  );
+}
+
+export interface CarouselProps {
+  images: string[];
+  slidesToShow: number;
+}
+
+export default function CaroueslSabores(props: CarouselProps) {
+  const container = useRef<HTMLDivElement>(null);
+  const [slideWidth, setSlideWidth] = useState(0);
+
+  const slidePaddingX = 8; // I.e. 0.5rem on each side.
+
+  useEffect(() => {
+    const containerWidth = container.current
+      ? container.current.offsetWidth
+      : 0;
+    setSlideWidth(
+      Math.floor(containerWidth / props.slidesToShow) - 2 * slidePaddingX
+    );
+  }, [container.current]);
+
+  const settings: Settings = {
+    dots: true,
+    infinite: true,
+    // fade: true,
+    speed: 500,
+    slidesToShow: props.slidesToShow,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PreviousArrow />,
+  };
+  return (
+    // Root box helps the carousel figure out how wide its container is.
+    // -80px is to make space for the two arrow buttons.
+    // mb is essential to show the dots.
+    <Box ref={container} sx={{ width: "calc(100% - 80px)", mb: 6 }}>
+      <div className="slider-container">
+        <Slider {...settings}>
+          {props.images.map((url, i) => (
+            <Box sx={{ my: "auto" }} key={i}>
+              <img
+                src={url}
+                style={{
+                  width: slideWidth,
+                  margin: "0 auto",
+                  transition: "all 400ms ease-in-out",
+                }}
+              />
+            </Box>
+          ))}
+        </Slider>
+      </div>
+    </Box>
+  );
+}
